cadab31ccbcf7a14209df545e84323ee7f95ac74
se2crid1 <151872490+se2crid@users.noreply.github.com>
2025-06-22 17:22:28 +0200

    Revert "Update LCSettingsView.swift"
    This reverts commit 76f82fc9d047260a28c749646efb6ec32569d4da.

--- LiveContainerSwiftUI/LCSettingsView.swift
+++ LiveContainerSwiftUI/LCSettingsView.swift
@@ -571,17 +571,14 @@ struct LCSettingsView: View {
     
     func importCertificateFromFeather() async {
         let callbackURL = "\(LCUtils.appUrlScheme())://receive-cert"
-        var comps = URLComponents()
-        comps.scheme = "feather"
-        comps.host = "export-certificate"
-        comps.queryItems = [URLQueryItem(name: "callback", value: callbackURL)]
-
-        guard let url = comps.url else {
-            errorInfo = "Failed to build Feather certificate request URL."
+        var allowed = CharacterSet.urlQueryAllowed
+        allowed.remove(charactersIn: ":/?")
+        guard let encodedCallback = callbackURL.addingPercentEncoding(withAllowedCharacters: allowed),
+              let url = URL(string: "feather://export-certificate?callback=\(encodedCallback)") else {
+            errorInfo = "Failed to initialize Feather certificate import URL."
             errorShow = true
             return
         }
-
         await UIApplication.shared.open(url)
     }
     
